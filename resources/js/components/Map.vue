399b71fe54cb091407b4a4bb7d2a1ebfef228ea9
James Matthews <james.ep.matthews@gmail.com>
2020-06-18 21:54:25 +0100

    Change to simplex noise

--- resources/js/components/Map.vue
+++ resources/js/components/Map.vue
@@ -63,6 +63,7 @@
 <script>
     //Using the Diamond Square Algorithm
     //https://en.wikipedia.org/wiki/Diamond-square_algorithm
+    let SimplexNoise = require('simplex-noise');
     export default {
         name: "Map",
         data() {
@@ -80,7 +81,7 @@
                 // dimension: 1024 * 3,
                 // canvasWidth: 1024 * 3,
 
-                dimension: 1024,
+                chunkSize: 1024,
                 canvasWidth: 1024,
 
                 unitSize: 1,
@@ -150,117 +151,27 @@
                 this.hasRun = true;
 
                 this.$worker.run((data) => {
-                    for (let x = 0; x < data.dimension + 1; x ++) { //width
-                        data.map[x] = [];
-                        for (let y = 0; y < data.dimension + 1; y ++) { //height
-                            if (50 > Math.floor(Math.random() * data.dimension)) {
-                                data.map[x][y] = 1;
-                            } else {
-                                data.map[x][y] = 0;
+                    let simplex = new SimplexNoise();
+                    function GetChunk(chunkX,chunkY)
+                    {
+                        let chunk = [];
+                        for(let x = 0; x < data.chunkSize; x++) {
+                            chunk[x] = [];
+                            for (let y = 0; y < data.chunkSize; y++) {
+                                chunk[x][y] = GetNoise(x + (chunkX * data.chunkSize), y + (chunkY * data.chunkSize));
                             }
                         }
+                        return chunk;
                     }
 
-                    function seed() {
-                        data.map[0][0] = Math.random();
-                        data.topLeft = data.map[0][0];
-
-                        // bottom left
-                        data.map[0][data.dimension] = Math.random();
-                        data.bottomLeft = data.map[0][data.dimension];
-
-                        // top right
-                        data.map[data.dimension][0] = Math.random();
-                        data.topRight = data.map[data.dimension][0];
-
-                        // bottom right
-                        data.map[data.dimension][data.dimension] = Math.random();
-                        data.bottomRight = data.map[data.dimension][data.dimension];
-
-                        //middle
-                        data.map[data.dimension / 2][data.dimension / 2] = data.map[0][0] + data.map[0][data.dimension] + data.map[data.dimension][0] + data.map[data.dimension][data.dimension] / 4;
-                        data.map[data.dimension / 2][data.dimension / 2] = normalize(data.map[data.dimension / 2][data.dimension / 2]);
-                        data.center = data.map[data.dimension / 2][data.dimension / 2];
-
-                        //Start displacement
-                        midpointDisplacement(data.dimension)
-                    }
-                    function midpointDisplacement(dimension) {
-                        //Half the original dimension
-                        let newDimension = 2 * Math.round(dimension / 2) - 2;
-
-                        //If we are now smaller than the unitsize then we are done
-                        if (newDimension <= data.unitSize) return;
-
-                        for (let i = newDimension; i <= data.dimension; i += newDimension) {
-                            for (let j = newDimension; j <= data.dimension; j += newDimension) {
-                                let x = i - (newDimension / 2);
-                                let y = j - (newDimension / 2);
-
-                                let topLeft = data.map[i - newDimension][j - newDimension];
-                                let topRight = data.map[i][j - newDimension];
-                                let bottomLeft = data.map[i - newDimension][j];
-                                let bottomRight = data.map[i][j];
-
-                                // Center
-                                data.map[x][y] = (topLeft + topRight + bottomLeft + bottomRight) / 4 + displace(dimension);
-                                data.map[x][y] = normalize(data.map[x][y]);
-                                let center = data.map[x][y];
-
-                                // Top
-                                if (j - (newDimension * 2) + (newDimension / 2) > 0) {
-                                    data.map[x][j - newDimension] = (topLeft + topRight + center + data.map[x][j - dimension + (newDimension / 2)]) / 4 + displace(dimension);
-                                } else {
-                                    data.map[x][j - newDimension] = (topLeft + topRight + center) / 3 + displace(dimension);
-                                }
-
-                                data.map[x][j - newDimension] = normalize(data.map[x][j - newDimension]);
-
-                                // Bottom
-                                if (j + (newDimension / 2) < data.dimension) {
-                                    data.map[x][j] = (bottomLeft + bottomRight + center + data.map[x][j + (newDimension / 2)]) / 4 + displace(dimension);
-                                } else {
-                                    data.map[x][j] = (bottomLeft + bottomRight + center) / 3 + displace(dimension);
-                                }
-
-                                data.map[x][j] = normalize(data.map[x][j]);
-
-                                //Right
-                                if (i + (newDimension / 2) < data.dimension) {
-                                    data.map[i][y] = (topRight + bottomRight + center + data.map[i + (newDimension / 2)][y]) / 4 + displace(dimension);
-                                } else {
-                                    data.map[i][y] = (topRight + bottomRight + center) / 3 + displace(dimension);
-                                }
-
-                                data.map[i][y] = normalize(data.map[i][y]);
-
-                                // Left
-                                if (i - (newDimension * 2) + (newDimension / 2) > 0) {
-                                    data.map[i - newDimension][y] = (topLeft + bottomLeft + center + data.map[i - dimension + (newDimension / 2)][y]) / 4 + displace(dimension);
-                                } else {
-                                    data.map[i - newDimension][y] = (topLeft + bottomLeft + center) / 3 + displace(dimension);
-                                }
-
-                                data.map[i - newDimension][y] = normalize(data.map[i - newDimension][y]);
-                            }
-                        }
-
-                        midpointDisplacement(newDimension);
-                    }
-                    function normalize(value) {
-                        return Math.max(Math.min(value, 1), 0);
-                    }
-                    function displace(number) {
-                        let max = number / (data.dimension) * data.roughness;
-
-                        let modifier = 0.5;
-
-                        let val = (Math.random() - modifier) * max;
-
-                        return val;
+                    function GetNoise(x,y){
+                        let noise = simplex.noise2D(x,y);
+                        let normalised = noise * 0.5 + 0.5;
+                        return normalised;
                     }
 
-                    seed();
+                    data.map = GetChunk(0,0);
+
                     return data.map;
                 }, [this._data])
                     .then(result => {
